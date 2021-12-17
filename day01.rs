cec12095585ffaed5fa8dbc953658b7301944487
Joe Profeta <japrofet@gmail.com>
2021-12-17 13:21:28 -0500

    Finish day 1 and 2 puzzles

---
+++ day01.rs
@@ -0,0 +1,88 @@
+//use std::vec;
+use std::convert::TryInto;
+use std::iter::FromIterator;
+
+mod helpers;
+mod day01_input;
+
+const TEST_INPUT: [i32; 10] = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+];
+const TEST_RESULT_PART1: i32 = 7;
+const TEST_RESULT_PART2: i32 = 5;
+
+struct Input(Vec<i32>);
+
+
+fn main() {
+    println!("Advent of Code");
+    println!("Day 01 - Sonar Sweep");
+    println!();
+
+    println!("Part 1");
+    println!("======");
+    println!();
+
+    let puzzle_test_out1 = do_part1(&Input(Vec::from(TEST_INPUT)));
+    println!("Test output: {} (expected {})", puzzle_test_out1, TEST_RESULT_PART1);
+    assert_eq!(TEST_RESULT_PART1, puzzle_test_out1);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out1 = do_part1(&Input(Vec::from(day01_input::PUZZLE_INPUT)));
+    println!("Puzzle result: {}", puzzle_out1);
+    println!();
+
+    println!("Part 2");
+    println!("======");
+    println!();
+
+    let test_out2 = do_part2(&Input(Vec::from(TEST_INPUT)));
+    println!("Test output: {} (expected {})", test_out2, TEST_RESULT_PART2);
+    assert_eq!(TEST_RESULT_PART2, test_out2);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out2 = do_part2(&Input(Vec::from(day01_input::PUZZLE_INPUT)));
+    println!("Puzzle result: {}", puzzle_out2);
+    println!();
+}
+
+fn do_part1(input: &Input) -> i32 {
+    let depths = &input.0;
+    let pairs = depths.iter()
+                    .take(depths.len()-1)
+                    .zip(depths.iter().skip(1));
+    pairs
+        .map(|x| x.1-x.0)
+        .filter(|x| *x > 0)
+        .count()
+        .try_into().unwrap()
+}
+
+fn do_part2(input: &Input) -> i32 {
+    let depths = &input.0;
+    let triples = depths.iter()
+                    .take(depths.len()-2)
+                    .zip(depths.iter().skip(1).take(depths.len()-1))
+                    .zip(depths.iter().skip(2))
+                    .map(|x| (x.0.0, x.0.1, x.1));
+    let sums = Vec::from_iter(triples.map(|x| x.2+x.1+x.0));
+    let sum_pairs = sums.iter()
+                    .take(sums.len()-1)
+                    .zip(sums.iter().skip(1));
+    sum_pairs
+        .map(|x| x.1-x.0)
+        .filter(|x| *x > 0)
+        .count()
+        .try_into().unwrap()
+}
---
+++ day02.rs
@@ -0,0 +1,94 @@
+use std::iter::FromIterator;
+use std::str::FromStr;
+
+mod day02_input;
+
+enum SubOperation {
+    Forward,
+    Down,
+    Up
+}
+
+struct Action { op: SubOperation, value: i32 }
+impl FromStr for Action {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(action_str: &str) -> Result<Self, Self::Err> {
+        let i = action_str.find(' ').unwrap();
+        let (op_s,val_s) = action_str.split_at(i);
+        let val = val_s.trim().parse::<i32>();
+        let op:Option<SubOperation> = match op_s {
+            "forward" => Some(SubOperation::Forward),
+            "down" => Some(SubOperation::Down),
+            "up" => Some(SubOperation::Up),
+            _ => None,
+        };
+        Ok(Action { op:op.unwrap(), value: val.unwrap() })
+    }
+}
+
+fn main() {
+    println!("Advent of Code");
+    println!("Day 02 - Dive");
+    println!();
+
+    println!("Part 1");
+    println!("======");
+    println!();
+
+    let puzzle_test_out1 = do_part1(&Vec::from_iter(day02_input::TEST_INPUT.iter().map(|x| Action::from_str(x).unwrap())));
+    println!("Test output: {} (expected {})", puzzle_test_out1, day02_input::TEST_RESULT_PART1);
+    assert_eq!(day02_input::TEST_RESULT_PART1, puzzle_test_out1);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out1 = do_part1(&Vec::from_iter(day02_input::PUZZLE_INPUT.iter().map(|x| Action::from_str(x).unwrap())));
+    println!("Puzzle result: {}", puzzle_out1);
+    println!();
+
+    println!("Part 2");
+    println!("======");
+    println!();
+
+    let test_out2 = do_part2(&Vec::from_iter(day02_input::TEST_INPUT.iter().map(|x| Action::from_str(x).unwrap())));
+    println!("Test output: {} (expected {})", test_out2, day02_input::TEST_RESULT_PART2);
+    assert_eq!(day02_input::TEST_RESULT_PART2, test_out2);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out2 = do_part2(&Vec::from_iter(day02_input::PUZZLE_INPUT.iter().map(|x| Action::from_str(x).unwrap())));
+    println!("Puzzle result: {}", puzzle_out2);
+    println!();
+}
+
+fn do_part1(actions: &Vec<Action>) -> i32 {
+    let position = 
+        actions.iter().fold(
+            (0,0),
+            |acc, a| {
+                let (hor, ver) = acc;
+                match a.op {
+                    SubOperation::Forward => (hor + a.value, ver),
+                    SubOperation::Down => (hor, ver + a.value),
+                    SubOperation::Up => (hor, ver - a.value)
+                }
+            }
+        );
+    position.0 * position.1
+}
+
+fn do_part2(actions: &Vec<Action>) -> i32 {
+    let position = 
+        actions.iter().fold(
+            (0,0,0),
+            |acc, a| {
+                let (hor, ver, aim) = acc;
+                match a.op {
+                    SubOperation::Forward => (hor + a.value, ver + aim*a.value, aim),
+                    SubOperation::Down => (hor, ver, aim + a.value),
+                    SubOperation::Up => (hor, ver, aim - a.value)
+                }
+            }
+        );
+    position.0 * position.1
+}
