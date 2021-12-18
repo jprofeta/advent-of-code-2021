be9980d71118d5afb315d4a070f8baaa30c1f4ec
Joe Profeta <japrofet@gmail.com>
2021-12-18 18:11:19 -0500

    Finish day 6

--- day06.rs
+++ day06.rs
@@ -9,16 +9,22 @@ use std::convert::TryInto;
 
 mod day06_input;
 
+const DAYS_PART1: i32 = 80;
+const DAYS_PART2: i32 = 256;
+const OLD_FISH_PERIOD: usize = 6;
+const NEW_FISH_PERIOD: usize = OLD_FISH_PERIOD + 2;
+
 #[derive(Debug)]
 struct InputError { }
-struct Input { }
+#[derive(Debug)]
+struct Input { fish: Vec<i32> }
 
 impl FromStr for Input {
-    type Err = InputError;
+    type Err = std::num::ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        
-
-        Ok(Input { })
+        let s = String::from(s);
+        let f: Vec<i32> = s.split(',').map(|x| x.trim().parse::<i32>().unwrap()).collect();
+        Ok(Input { fish: f })
     }
 }
 
@@ -56,10 +62,39 @@ fn main() {
     println!();
 }
 
+// This is pretty ineffecient way to go about it. Part 2 is much better.
 fn do_part1(input: Input) -> i32 {
-    0
+    let mut fish = input.fish;
+    for i in 0..DAYS_PART1 {
+        let new_fish = fish.iter().filter(|x| **x == 0).count();
+        fish = fish.iter().map(|x| { let x = x - 1; if x < 0 { OLD_FISH_PERIOD as i32} else { x } }).collect();
+        fish.append(&mut vec![NEW_FISH_PERIOD as i32; new_fish]);
+    }
+    fish.len() as i32
 }
 
-fn do_part2(input: Input) -> i32 {
-    0
+fn do_part2(input: Input) -> i64 {
+    let fish = input.fish;
+    let mut fish_at_age: Vec<i64> = vec![0; NEW_FISH_PERIOD+1];
+
+    // Initialize the fish age counts
+    for i in 0..=OLD_FISH_PERIOD {
+        fish_at_age[i] = fish.iter().filter(|x| **x == (i as i32)).count() as i64;
+    }
+
+    for d in 0..DAYS_PART2 {
+        // Move the fish counts down to the next day
+        // Save the zero fish before moving since it is needed to modify the array
+        let day0_fish = fish_at_age[0];
+        for i in 1..=NEW_FISH_PERIOD {
+            fish_at_age[i-1] = fish_at_age[i];
+        }
+        // Birth some fish equal to the number of day0 fish
+        fish_at_age[NEW_FISH_PERIOD] = day0_fish;
+        // Now add the day0 fish back into the count
+        fish_at_age[OLD_FISH_PERIOD] += day0_fish;
+    }
+
+    // Add up all the fish
+    fish_at_age.iter().sum::<i64>()
 }
