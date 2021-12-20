ad2e9956d67c5c33a50a2eda71eed432b110c005
Joe Profeta <japrofet@gmail.com>
2021-12-20 12:23:24 -0500

    Create day 10 files

---
+++ day10.rs
@@ -0,0 +1,66 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::iter::FromIterator;
+use std::iter::IntoIterator;
+use std::str::FromStr;
+use std::convert::TryInto;
+
+mod day10_input;
+
+#[derive(Debug)]
+struct InputError { }
+#[derive(Debug)]
+struct Input { }
+
+impl FromStr for Input {
+    type Err = InputError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        
+
+        Ok(Input { })
+    }
+}
+
+fn main() {
+    println!("Advent of Code 2021");
+    println!("Day 10 - Syntax Scoring");
+    println!();
+
+    println!("Part 1");
+    println!("======");
+    println!();
+
+    let puzzle_test_out1 = do_part1(day10_input::TEST_INPUT.parse::<Input>().unwrap());
+    println!("Test output: {} (expected {})", puzzle_test_out1, day10_input::TEST_RESULT_PART1);
+    assert_eq!(day10_input::TEST_RESULT_PART1, puzzle_test_out1);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out1 = do_part1(day10_input::PUZZLE_INPUT.parse::<Input>().unwrap());
+    println!("Puzzle result: {}", puzzle_out1);
+    println!();
+
+    println!("Part 2");
+    println!("======");
+    println!();
+
+    let test_out2 = do_part2(day10_input::TEST_INPUT.parse::<Input>().unwrap());
+    println!("Test output: {} (expected {})", test_out2, day10_input::TEST_RESULT_PART2);
+    assert_eq!(day10_input::TEST_RESULT_PART2, test_out2);
+
+    println!();
+    println!("Running puzzle input...");
+    let puzzle_out2 = do_part2(day10_input::PUZZLE_INPUT.parse::<Input>().unwrap());
+    println!("Puzzle result: {}", puzzle_out2);
+    println!();
+}
+
+fn do_part1(input: Input) -> i32 {
+    0
+}
+
+fn do_part2(input: Input) -> i32 {
+    0
+}
