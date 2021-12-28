44da44ffd80fe760fb38902403fffa0a950f4526
Joe Profeta <japrofet@gmail.com>
2021-12-28 00:13:23 -0500

    Add input parsing for day 13

--- day13.rs
+++ day13.rs
@@ -21,14 +21,43 @@ enum FoldDirection {
 #[derive(Debug)]
 struct InputError { }
 #[derive(Debug)]
-struct Input { dots: Vec<(i32, i32)>, folds: Vec<(FoldDirection, i32)> }
+struct Input { dots: Vec<(usize, usize)>, folds: Vec<(FoldDirection, usize)> }
 
 impl FromStr for Input {
     type Err = InputError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        
+        enum InputState { Dots, Folds }
 
-        Ok(Input { })
+        let s = String::from(s);
+        let mut state = InputState::Dots;
+
+        let mut dots: Vec<(usize, usize)> = Vec::new();
+        let mut folds: Vec<(FoldDirection, usize)> = Vec::new();
+
+        for l in s.lines() {
+            match state {
+                InputState::Dots => {
+                    let l = l.trim();
+                    if l.len() > 0 {
+                        let coords = l.trim().split(',').collect::<Vec<&str>>();
+                        dots.push((coords[0].parse::<usize>().unwrap(), coords[1].parse::<usize>().unwrap()));
+                    } else {
+                        state = InputState::Folds;
+                    }
+                },
+                InputState::Folds => {
+                    let l = l.trim().strip_prefix("fold along ").unwrap();
+                    let line = l.split('=').collect::<Vec<&str>>();
+                    if line[0] == "x" {
+                        folds.push((FoldDirection::Vertical, line[1].parse::<usize>().unwrap()));
+                    } else if line[0] == "y" {
+                        folds.push((FoldDirection::Horizontal, line[1].parse::<usize>().unwrap()));
+                    }
+                }
+            }
+        }
+
+        Ok(Input { dots, folds })
     }
 }
 
